9e056f648ebbc9d25dc268cc5ffcb55465cae5c4
Hao Xie <vifim@qq.com>
2023-02-25 15:28:29 +0000

    rename

---
+++ src/uk/ac/gla/dcs/bigdata/studentstructures/NewsTextualDistanceInNeed.java
@@ -0,0 +1,42 @@
+package uk.ac.gla.dcs.bigdata.studentstructures;
+
+import java.io.Serializable;
+
+public class NewsTextualDistanceInNeed implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    String id; // unique article identifier
+    String title;
+    Double dphScore; // DPH score of article
+
+    public NewsTextualDistanceInNeed(String id, String title, Double dphScore) {
+        this.id = id;
+        this.title = title;
+        this.dphScore = dphScore;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Double getDphScore() {
+        return dphScore;
+    }
+
+    public void setDphScore(Double dphScore) {
+        this.dphScore = dphScore;
+    }
+
+}
